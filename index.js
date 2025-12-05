643f3269ba97dac013340340d618fd7e78064397
G v s Harsha Vardhan Reddy <227r1a6222@gmail.com>
2025-12-05 22:59:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const myServer = http.createServer((req,res) =>{
+    console.log("Request Received");
+    res.end("Hello Server");
+});
+
+myServer.listen(8000,()=>{
+    console.log("Server Started");
+})
